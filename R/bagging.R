162d8a3ff57ecee5a9465587faf55754b1e3cea9
Shannon Rush <shannonmrush@gmail.com>
2013-03-22 17:11:24 -0600

    pulling out the last of project specific code from bagging, adding bagging tests

--- R/bagging.R
+++ R/bagging.R
@@ -1,12 +1,15 @@
 library(randomForest)
 
-bagging <- function(n,train_set,test_set,ntree=500) {
-  m <- matrix(nrow=nrow(test_set),ncol=n)
-  for (i in 1:n) {
-    model <- ifelse(type %in% types,type,sample(types,1))
-    t <- train_set[sample(nrow(train_set), nrow(train_set),replace=T), ]
-    fit <- randomForest(activity~.,data=t,ntree=ntree,do.trace=T)      
-    m[,i] <- as.character(predict(fit,test_set))
+bagging <- function(formula,m,data,test,ntree=500,mtry=NULL,trace=T) {
+  outcome.label <- outcomeLabel(formula)
+  mtry <- ifelse(is.null(mtry),floor(sqrt(ncol(data))),mtry)
+  C <- matrix(nrow=nrow(test),ncol=m)
+  n <- nrow(data)
+  for (i in 1:m) {
+    t <- data[sample(n, n, replace=T), ]
+    t[,outcome.label]<-droplevels(t[,outcome.label]) # in case any outcomes are not sampled
+    fit <- randomForest(formula,data=t,ntree=ntree,do.trace=trace)      
+    C[,i] <- as.character(predict(fit,test))
   }
   apply(m,1,mostOccurring)
 }
@@ -22,6 +25,10 @@ numMost <- function(sample) {
 
 namesMost <- function(sample) {
   names(which(table(sample)==numMost(sample)))
+}
+
+outcomeLabel <- function(formula) {
+  toString(formula[[2]])
 }
 
 
